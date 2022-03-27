20cc052da5dc43bc4652988e74eac2b77d99b81f
Marcelo Camillo <marcelocamillo18@gmail.com>
2022-03-26 21:42:27 -0300

    Inserindo dados

--- index.js
+++ index.js
@@ -16,6 +16,24 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 
+app.get('/users/create', (req, res) => {
+  res.render('adduser');
+});
+
+app.post('/users/create', async (req, res) => {
+  const name = req.body.name;
+  const occupation = req.body.occupation;
+  let newsletter = req.body.newsletter;
+
+  newsletter === 'on' ? (newsletter = true) : (newsletter = false);
+
+  console.log(req.body);
+
+  await User.create({ name, occupation, newsletter });
+
+  res.redirect('/');
+});
+
 conn
   .sync()
   .then(() => {
